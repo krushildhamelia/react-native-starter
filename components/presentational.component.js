f3813fc79617ae9ca0d5f8f8e351cf0045abb0d5
krushil.dhamelia <krushil1511995@gmail.com>
2018-03-13 08:55:54 +0530

    Styling React Native

--- components/presentational.component.js
+++ components/presentational.component.js
@@ -1,5 +1,5 @@
 import React,{ Component } from "react";
-import {Text , View } from 'react-native';
+import {Text , View, StyleSheet } from 'react-native';
 
 
 
@@ -7,10 +7,47 @@ export default class Presentational extends Component{
     render(){
         return (
             <View>
-                <Text onPress={ this.props.updateState}>
+                <Text style={styles.persent} onPress={ this.props.updateState}>
                     { this.props.myState }
                 </Text>
+                <View style={styles.container} >
+                    <View style={styles.yellowBox}></View>
+                    <View style={styles.redBox}></View>
+                    <View style={styles.blueBox}></View>
+                    <View style={styles.blueBox}></View>
+                </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    persent : {
+        textAlign : 'center',
+        color : 'red',
+        fontWeight: 'bold',
+        fontSize : 20
+    },
+    container:{
+        flexDirection : 'column',
+        justifyContent  :'center',
+        alignItems : 'center',
+        // backgroundColor: 'grey',s
+    },
+    redBox : {
+        width : 100,
+        height : 100,
+        backgroundColor: 'black'
+    },
+    yellowBox : {
+        width : 100,
+        height : 100,
+        backgroundColor: 'yellow',
+        borderColor: "transparent"
+    },
+    blueBox : {
+        width : 100,
+        height : 100,
+        backgroundColor: 'blue'
+    }
+});
